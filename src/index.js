2e9204dbedd6225a6c0aabd64a340726851711f4
SampoLS <mr.sampo@outlook.com>
2022-03-28 22:36:01 +0800

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
+
 import App from "./App";
 import { store } from "./store/store";
+import UserDetails from "./components/UserDetails";
 
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UserDetails from "./components/UserDetails";
 
 ReactDOM.render(
   <React.StrictMode>
